99fc6c0ab53df4723a7098281f11bb48c9090b0a
dev30k <develop_kk@outlook.com>
2023-05-30 10:33:48 +0300

    double linked list not fully complete yet

--- doubleLinkedList/doubleLinkedList.go
+++ doubleLinkedList/doubleLinkedList.go
@@ -1,22 +1,38 @@
 package doubleLinkedList
 
+import "fmt"
+
 type Node struct {
 	Value interface{}
 	Next  *Node
 	Prev  *Node
 }
 
-type LinkedList struct {
+type DoubleLinkedList struct {
 	Head *Node
+	Tail *Node
 }
 
-func (ll *LinkedList) InsertAtBeginning(value interface{}) {
-	newNode := &Node{Value: value, Next: ll.Head, Prev: nil}
+func (ll *DoubleLinkedList) InsertAtBeginning(value interface{}) {
+	newNode := &Node{Value: value, Prev: nil, Next: ll.Head}
+	if ll.Head != nil {
+		ll.Head.Prev = newNode
+	}
 	ll.Head = newNode
-
+	if ll.Tail == nil {
+		ll.Tail = newNode
+	}
 }
+func (ll *DoubleLinkedList) TraverseLinkedList(fn func(value interface{})) {
+	currentNode := ll.Head
+	for currentNode.Next != nil {
+		fn(currentNode.Value)
+		fmt.Println()
+		currentNode = currentNode.Next
 
-func (ll *LinkedList) InsertAtPosition(value interface{}, position int) {
+	}
+}
+func (ll *DoubleLinkedList) InsertAtPosition(value interface{}, position int) {
 
 	newNode := &Node{Value: value}
 	current := ll.Head
@@ -51,7 +67,12 @@ func (ll *LinkedList) InsertAtPosition(value interface{}, position int) {
 	current.Next = newNode
 
 }
+func (ll *DoubleLinkedList) InsertAtEnd(value interface{}) {
+	newNode := &Node{Value: value}
+	current := ll.Head
 
-func (ll *LinkedList) InsertAtEnd(value interface{}) {
-	
+	for current.
+
+
+
 }
--- doubleLinkedList/doubleLinkedList_test.go
+++ doubleLinkedList/doubleLinkedList_test.go
@@ -1,10 +1,34 @@
-package doubleLinkedList
+package doubleLinkedList_test
 
 import (
+	"docker-test/doubleLinkedList"
+	"fmt"
 	"testing"
-	"docker-test/"
 )
 
+// there is a problem here that i cannot see
 func TestLinkedList_InsertAtBeginning(t *testing.T) {
-	ll := &LinkedList.
+	ll := &doubleLinkedList.DoubleLinkedList{}
+
+	ll.InsertAtBeginning("World")
+	ll.InsertAtBeginning("Hello")
+	ll.InsertAtBeginning("Hello")
+	values := make([]string, 0)
+	ll.TraverseLinkedList(func(value interface{}) {
+
+		values = append(values, value.(string))
+		fmt.Println(values)
+	})
+	fmt.Println(values)
+	expected := []string{"Hello", "Hello"}
+	if len(values) != len(expected) {
+		t.Errorf("Unexpected linked list length: got %d, want %d", len(values), len(expected))
+	}
+
+	for i := range values {
+		if values[i] != expected[i] {
+			t.Errorf("Unexpected value at position %d: got %s, want %s", i, values[i], expected[i])
+		}
+	}
+
 }
